dff5a27c4aa89d45bce4fb6ab2992ed8ac5c4acd
Toby McDonald <toby_mcdonald@email.com>
2022-02-26 17:40:39 -0500

    create database

--- clientServices/newUsersService/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-	echo "Successfully deployed the demo app to ".$_SERVER['SERVER_NAME'];
-?>
--- www/techDemoTesterThing.php
+++ www/techDemoTesterThing.php
@@ -1,14 +1,64 @@
 <?php
 $serviceToCreate = "DemoApp";
 $newServiceName = "newUsersService";
-createService($serviceToCreate, $newServiceName);
-
-function createService($service, $name) {
-
-	if (copyDir("../serviceTemplates/".$service, "../clientServices/".$name)) {
+try {
+	if (createService($serviceToCreate, $newServiceName)) {
 		echo "createdSite";
 	}
+} catch (Exception $ex) {
+	echo $ex->getMessage();
+}
 
+function createService($service, $name) {
+	$name = strtolower(sanitize($name));
+	if (!file_exists("../clientServices/".$name)) {
+		if (copyDir("../serviceTemplates/".$service, "../clientServices/".$name) && createDB($name)) {
+			return true;
+		}
+	} else {
+		throw new Exception("Error: Service Already Exists With Name: ".$name, 100);
+	}
+}
+
+function sanitize($str) {
+	return str_replace(Array("\\","/","'","\""), "", $str);
+}
+
+function createDB($name) {
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$conn = new mysqli($servername, $username, $password);
+	if ($conn->connect_error) {
+		throw new Exception("Error: SQL Connection failed: ".$conn->connect_error, 100);
+	}
+	$newdbname = "CSD_".$name;
+	$newusername = "CSU_".$name;
+	$newpassword = generatePswrd();
+	$newdbname = $conn->real_escape_string($newdbname);
+	$newusername = $conn->real_escape_string($newusername);
+	$sql = "CREATE USER '".$newusername."'@'".$servername."' IDENTIFIED BY '".$newpassword."';";
+	$sql .= "CREATE DATABASE ".$newdbname.";";
+	$sql .= "GRANT ALL PRIVILEGES ON '".$newdbname."'.* TO '".$newusername."'@'".$servername."'";
+	if ($conn->multi_query($sql) === TRUE) {
+		//write to db.php here
+	} else {
+		throw new Exception("Error: Error creating database: " . $conn->error, 100);
+	}
+	$conn->close();
+	return true;
+}
+
+function generatePswrd($length = 64, $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_+=~:,.<>?') {
+    $str = '';
+    $max = mb_strlen($keyspace, '8bit') - 1;
+    if ($max < 1) {
+        throw new Exception('$keyspace must be at least two characters long');
+    }
+    for ($i = 0; $i < $length; ++$i) {
+        $str .= $keyspace[random_int(0, $max)];
+    }
+    return $str;
 }
 
 function copyDir($src, $dst) {
